a51b5be32dd34cff9d420eb846a58d05f96124e0
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2023-01-26 23:52:33 +0100

    testt

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function calculateTax(income) {
+    if (income < 50000) {
+        return income * 1.2;
+    }
+    return income * 1.3;
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+type Employee = {
+   readonly id: number,
+   name: string,
+   retire: (date: Date) => void
+   
+}
+let employee: Employee ={
+   id: 1, 
+   name: "John",
+retire: (date: Date) => {
+   console.log(date)
+}};
